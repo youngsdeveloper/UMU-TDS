4e17f103e2503a7b0b4f282bb84e9f0f3a988724
Alumno <kiketos1999@gmail.com>
2021-10-18 19:03:19 +0200

    Test: Pantalla login v1

--- src/gui/VentanaLogin.java
+++ src/gui/VentanaLogin.java
@@ -2,19 +2,48 @@ package gui;
 
 import javax.swing.JPanel;
 import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.BoxLayout;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import java.awt.FlowLayout;
 
 public class VentanaLogin extends JPanel {
 
 	
 	public final static String TAG = "VentanaLogin";
+	private JTextField txtContrasea;
+	private JTextField textField;
 	
 	/**
 	 * Create the panel.
 	 */
 	public VentanaLogin() {
+		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 		
-		JLabel lblLogin = new JLabel("LOGIN!");
-		add(lblLogin);
+		JPanel panel = new JPanel();
+		add(panel);
+		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+		
+		JLabel lblNombreDeUsuario = new JLabel("Nombre de Usuario: ");
+		panel.add(lblNombreDeUsuario);
+		
+		txtContrasea = new JTextField();
+		txtContrasea.setText("Contraseña");
+
+		panel.add(txtContrasea);
+		txtContrasea.setColumns(10);
+		
+		JLabel lblContrasea = new JLabel("Contraseña: ");
+		panel.add(lblContrasea);
+		
+		textField = new JTextField();
+		panel.add(textField);
+		textField.setColumns(10);
+		
+		JPanel panel_1 = new JPanel();
+		add(panel_1);
 
 	}
 
--- src/gui/VentanaPrincipal.java
+++ src/gui/VentanaPrincipal.java
@@ -57,91 +57,91 @@ public class VentanaPrincipal {
 		frame = new JFrame();
 		frame.setBounds(100, 100,775, 800);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-				frame.getContentPane().setLayout(new BorderLayout(0, 0));
-				
-				JPanel panel = new JPanel();
-				frame.getContentPane().add(panel, BorderLayout.NORTH);
-				panel.setLayout(new FlowLayout(FlowLayout.LEFT));
-				
-				JLabel lblAppvideologo = new JLabel("AppVideoLogo");
-				panel.add(lblAppvideologo);
-				
-				panel.add(Box.createRigidArea(new Dimension(30,40)));
-				
-				JLabel lblNewLabel = new JLabel("Hola Jose Ramón");
-				panel.add(lblNewLabel);
-				
-				Component rigidArea = Box.createRigidArea(new Dimension(30, 40));
-				panel.add(rigidArea);
-				
-				JPanel panel_2 = new JPanel();
-				panel.add(panel_2);
-				
-				JButton btnRegistro = new JButton("Registro");
-				btnRegistro.addActionListener(new ActionListener() {
+			frame.getContentPane().setLayout(new BorderLayout(0, 0));
+			
+			JPanel panel = new JPanel();
+			frame.getContentPane().add(panel, BorderLayout.NORTH);
+			panel.setLayout(new FlowLayout(FlowLayout.LEFT));
+			
+			JLabel lblAppvideologo = new JLabel("AppVideoLogo");
+			panel.add(lblAppvideologo);
+			
+			panel.add(Box.createRigidArea(new Dimension(30,40)));
+			
+			JLabel lblNewLabel = new JLabel("Hola Jose Ramón");
+			panel.add(lblNewLabel);
+			
+			Component rigidArea = Box.createRigidArea(new Dimension(30, 40));
+			panel.add(rigidArea);
+			
+			JPanel panel_2 = new JPanel();
+			panel.add(panel_2);
+			
+			JButton btnRegistro = new JButton("Registro");
+			btnRegistro.addActionListener(new ActionListener() {
 
-					@Override
-					public void actionPerformed(ActionEvent arg0) {
-						cl.show(cards, VentanaRegistro.TAG);
-					}});
-				
-				panel_2.add(btnRegistro);
-				
-				JButton btnLogin = new JButton("Login");
-				btnLogin.addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent arg0) {
+					cl.show(cards, VentanaRegistro.TAG);
+				}});
+			
+			panel_2.add(btnRegistro);
+			
+			JButton btnLogin = new JButton("Login");
+			btnLogin.addActionListener(new ActionListener() {
 
-					@Override
-					public void actionPerformed(ActionEvent arg0) {
-						cl.show(cards, VentanaLogin.TAG);
-					}
-					
-				});
-				panel_2.add(btnLogin);
+				@Override
+				public void actionPerformed(ActionEvent arg0) {
+					cl.show(cards, VentanaLogin.TAG);
+				}
 				
-				Component rigidArea_1 = Box.createRigidArea(new Dimension(30, 40));
-				panel.add(rigidArea_1);
-				
-				JButton btnCerrarSesin = new JButton("Logout");
-				panel.add(btnCerrarSesin);
-				
-				Component rigidArea_1_1 = Box.createRigidArea(new Dimension(30, 40));
-				panel.add(rigidArea_1_1);
-				
-				JButton btnNewButton_2 = new JButton("Premium");
-				panel.add(btnNewButton_2);
-				
-				JPanel panel_1 = new JPanel();
-				frame.getContentPane().add(panel_1, BorderLayout.CENTER);
-				panel_1.setLayout(new BoxLayout(panel_1, BoxLayout.Y_AXIS));
-				
-				
-				JPanel panel_3 = new JPanel();
-				FlowLayout flowLayout = (FlowLayout) panel_3.getLayout();
-				flowLayout.setAlignment(FlowLayout.LEFT);
-				panel_1.add(panel_3);
-				
-				JButton btnNewButton = new JButton("Explorar");
-				panel_3.add(btnNewButton);
-				
-				JButton btnNewButton_1 = new JButton("Mis listas");
-				panel_3.add(btnNewButton_1);
-				
-				JButton btnNewButton_3 = new JButton("Recientes");
-				panel_3.add(btnNewButton_3);
-				
-				JButton btnNewButton_4 = new JButton("Nueva lista");
-				panel_3.add(btnNewButton_4);
-				
-				//Create the "cards".
-				JPanel card_login = new VentanaLogin();
-				JPanel card_registro = new VentanaRegistro();
-				
-				//Create the panel that contains the "cards".
-				cl = new CardLayout();
-				cards = new JPanel(cl);
-				panel_1.add(cards);
-				cards.add(card_login, VentanaLogin.TAG);
-				cards.add(card_registro, VentanaRegistro.TAG);
+			});
+			panel_2.add(btnLogin);
+			
+			Component rigidArea_1 = Box.createRigidArea(new Dimension(30, 40));
+			panel.add(rigidArea_1);
+			
+			JButton btnCerrarSesin = new JButton("Logout");
+			panel.add(btnCerrarSesin);
+			
+			Component rigidArea_1_1 = Box.createRigidArea(new Dimension(30, 40));
+			panel.add(rigidArea_1_1);
+			
+			JButton btnNewButton_2 = new JButton("Premium");
+			panel.add(btnNewButton_2);
+			
+			JPanel panel_1 = new JPanel();
+			frame.getContentPane().add(panel_1, BorderLayout.CENTER);
+			panel_1.setLayout(new BoxLayout(panel_1, BoxLayout.Y_AXIS));
+			
+			
+			JPanel panel_3 = new JPanel();
+			FlowLayout flowLayout = (FlowLayout) panel_3.getLayout();
+			flowLayout.setAlignment(FlowLayout.LEFT);
+			panel_1.add(panel_3);
+			
+			JButton btnNewButton = new JButton("Explorar");
+			panel_3.add(btnNewButton);
+			
+			JButton btnNewButton_1 = new JButton("Mis listas");
+			panel_3.add(btnNewButton_1);
+			
+			JButton btnNewButton_3 = new JButton("Recientes");
+			panel_3.add(btnNewButton_3);
+			
+			JButton btnNewButton_4 = new JButton("Nueva lista");
+			panel_3.add(btnNewButton_4);
+			
+			//Create the "cards".
+			JPanel card_login = new VentanaLogin();
+			JPanel card_registro = new VentanaRegistro();
+			
+			//Create the panel that contains the "cards".
+			cl = new CardLayout();
+			cards = new JPanel(cl);
+			panel_1.add(cards);
+			cards.add(card_login, VentanaLogin.TAG);
+			cards.add(card_registro, VentanaRegistro.TAG);
 				
 				
 
